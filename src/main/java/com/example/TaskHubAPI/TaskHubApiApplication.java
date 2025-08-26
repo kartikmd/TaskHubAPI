478487ee222e89ed7a651fc93f87e55d3f47c1bc
kartikmd <97731024+kartikmd@users.noreply.github.com>
2025-08-26 17:57:00 +0530

    updated

--- src/main/java/com/example/TaskHubAPI/TaskHubApiApplication.java
+++ src/main/java/com/example/TaskHubAPI/TaskHubApiApplication.java
@@ -7,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class TaskHubApiApplication {
 
 	public static void main(String[] args) {
-
 		SpringApplication.run(TaskHubApiApplication.class, args);
 		System.out.println("hi karthik");
 	}
--- src/main/java/com/example/TaskHubAPI/taskhub/Entity/TaskItem.java
+++ src/main/java/com/example/TaskHubAPI/taskhub/Entity/TaskItem.java
@@ -2,23 +2,24 @@ package com.example.TaskHubAPI.taskhub.Entity;
 
 import jakarta.persistence.*;
 import com.example.TaskHubAPI.taskhub.Enum.Status;
+import com.example.TaskHubAPI.taskhub.Enum.Priority;
 import lombok.Getter;
 import lombok.Setter;
-import com.example.TaskHubAPI.taskhub.Enum.Priority;
-
 import java.time.LocalDate;
+
 @Getter
 @Setter
 @Entity
-public class TaskItem
-{
+public class TaskItem {
+
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private long id;
-    private  String title;
-    private  String description;
 
-@Enumerated(EnumType.STRING)
+    private String title;
+    private String description;
+
+    @Enumerated(EnumType.STRING)
     private Status status;
 
     @Enumerated(EnumType.STRING)
